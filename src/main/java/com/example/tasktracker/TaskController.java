af3c86af5e0f9594f8596638050a70317e3e47e6
ANIR369 <anirudhtemkar@gmail.com>
2026-05-23 10:54:09 +0000

    Updated project files and Jenkinsfile

--- src/main/java/com/example/tasktracker/TaskController.java
+++ src/main/java/com/example/tasktracker/TaskController.java
@@ -1,34 +1,15 @@
 package com.example.tasktracker;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
+@Controller
 public class TaskController {
-
-    List<String> tasks = new ArrayList<>();
 
     @GetMapping("/")
     public String home() {
-        return "Daily Task Tracker Running";
-    }
 
-    @GetMapping("/tasks")
-    public List<String> getTasks() {
-        return tasks;
-    }
+        return "index";
 
-    @GetMapping("/add")
-    public String addTask(@RequestParam String task) {
-        tasks.add(task);
-        return "Task Added";
-    }
-
-    @GetMapping("/delete")
-    public String deleteTask(@RequestParam int index) {
-        tasks.remove(index);
-        return "Task Deleted";
     }
 }
